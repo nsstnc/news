d97f468022babf1f90b8a475037dfa190d6d6fba
nsstnc <egorgolubev0484@gmail.com>
2024-05-16 17:33:08 +0300

    Added articles

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eacff1361ab2ddcbd8922cea4b8cab164ac7a52e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66717c31f6496dc544165cfa69e5a2bf487c1fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
